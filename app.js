a7894c8fa6dbf0f3d34b2202c0af1246390c293b
Asad-Ghouri <asadghouri546@email.com>
2023-09-09 14:47:42 -0700

    .

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ const port = 5000;
 app.use(express.json());
 app.use(require('./Router/auth.js'));
 
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://payment-asad-ghouri.vercel.app');
+    // You can also specify additional CORS headers as needed.
+    // ...
+    next();
+  });
+  
 
 app.listen(port,() =>{
     console.log(`Sever is running at localhost ${port}`);  
