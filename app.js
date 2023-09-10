168c22110a1da9df1f744943a7abfafb3429af3f
Samama Aziz <122434021+Samama-Aziz@users.noreply.github.com>
2023-09-10 13:30:14 +0500

    Samama Updated app.js

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ require('./DB/connection');
 
 app.use(express.json());
 
-// Enable CORS for all routes
-app.use(cors());
+// Define CORS options
+const corsOptions = {
+  origin: 'https://alpha-payment-frontend.vercel.app',
+  methods: 'GET,PUT,POST,DELETE',
+  credentials: true, // Enable credentials (cookies, HTTP authentication) if needed
+  optionsSuccessStatus: 204, // Respond to preflight requests with a 204 status code
+};
+
+// Enable CORS for all routes using the defined options
+app.use(cors(corsOptions));
 
 app.use('/api', require('./Router/auth.js'));
 
