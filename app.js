be786792a11ab444c2a559095f5f653cf0406d3d
Samama Aziz <122434021+Samama-Aziz@users.noreply.github.com>
2023-09-10 13:26:01 +0500

    Samama Updated app.js

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
 const express = require('express');
-const app =express();
-const doteenv =require('dotenv');
-const cors = require('cors'); // Import the CORS middleware
-
-doteenv.config({path: './config.env'});
-require('./DB/connection');
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
 const port = 5000;
 
+dotenv.config({ path: './config.env' });
+require('./DB/connection');
+
 app.use(express.json());
-app.use(require('./Router/auth.js'));
 
-// const corsOptions = {
-//     origin: '*',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true, // Enable credentials (cookies, HTTP authentication) if needed
-//     optionsSuccessStatus: 204, // Respond to preflight requests with a 204 status code
-//   };
-  
-//   app.use(cors(corsOptions)); // 
+// Enable CORS for all routes
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://alpha-payment-frontend.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200); // Respond OK to OPTIONS requests
-  } else {
-    next();
-  }
-  });
+app.use('/api', require('./Router/auth.js'));
 
-app.listen(port,() =>{
-    console.log(`Sever is running at localhost ${port}`);  
-})
+app.listen(port, () => {
+  console.log(`Server is running at localhost:${port}`);
+});
