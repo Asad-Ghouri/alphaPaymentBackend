10123027f7b1efb0099bc4e79f7726e35b29b82e
Asad-Ghouri <asadghouri546@email.com>
2023-09-10 00:00:22 -0700

    .

--- app.js
+++ app.js
@@ -10,14 +10,22 @@ const port = 5000;
 app.use(express.json());
 app.use(require('./Router/auth.js'));
 
-const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Enable credentials (cookies, HTTP authentication) if needed
-    optionsSuccessStatus: 204, // Respond to preflight requests with a 204 status code
-  };
+// const corsOptions = {
+//     origin: '*',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true, // Enable credentials (cookies, HTTP authentication) if needed
+//     optionsSuccessStatus: 204, // Respond to preflight requests with a 204 status code
+//   };
   
-  app.use(cors(corsOptions)); // 
+//   app.use(cors(corsOptions)); // 
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 
 app.listen(port,() =>{
     console.log(`Sever is running at localhost ${port}`);  
