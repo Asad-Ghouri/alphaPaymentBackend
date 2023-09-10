77a24362e3d4c47fc1ed6a81de6b4e4a599d3cbb
Asad-Ghouri <asadghouri546@email.com>
2023-09-10 00:05:07 -0700

    .

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(require('./Router/auth.js'));
 //   app.use(cors(corsOptions)); // 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
